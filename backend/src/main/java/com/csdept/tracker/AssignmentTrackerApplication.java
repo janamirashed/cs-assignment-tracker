a78ecca6a55aa3cbb248ab11607f360416b9effd
janamirashed <janarashed23010359@gmail.com>
2026-03-16 17:18:52 +0200

    Initial Project Structure

---
+++ backend/src/main/java/com/csdept/tracker/AssignmentTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.csdept.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentTrackerApplication.class, args);
+    }
+}
